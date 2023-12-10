a3cf7f5b75e1ec0ffbc5abb12fe88cb318bf0c7d
Vladimir-Kharchenko3 <141916443+Vladimir-Kharchenko3@users.noreply.github.com>
2023-12-10 19:43:57 +0300

    Решил задачу 3

---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем произвольную строку
+        string inputString = "A man, a plan, a canal, Panama";
+
+        // Проверяем, является ли строка палиндромом
+        bool isPalindrome = IsPalindrome(inputString);
+
+        // Выводим результат
+        if (isPalindrome)
+        {
+            Console.WriteLine("Строка является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Строка не является палиндромом.");
+        }
+    }
+
+    static bool IsPalindrome(string input)
+    {
+        // Удаляем из строки все символы, не являющиеся буквами
+        string cleanedString = new string(input
+            .ToLower()
+            .ToCharArray()
+            .Where(c => Char.IsLetter(c))
+            .ToArray());
+
+        // Сравниваем очищенную строку с ее инвертированной копией
+        return cleanedString == new string(cleanedString.Reverse().ToArray());
+    }
+}
